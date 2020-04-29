8201ded9e72fb84b74689f3ca20b96cb469cc424
HamishWHC <hamishwhc@gmail.com>
2020-04-29 17:55:12 +1000

    Added CORS to fix issues with CORS.

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 from .config import BaseConfig
 
@@ -12,6 +13,7 @@ db = SQLAlchemy()
 migrate = Migrate()
 jwt_manager = JWTManager()
 marshmallow = Marshmallow()
+cors = CORS()
 
 
 def create_app(config: Type[BaseConfig] = BaseConfig) -> Flask:
@@ -22,6 +24,7 @@ def create_app(config: Type[BaseConfig] = BaseConfig) -> Flask:
     migrate.init_app(app, db)
     jwt_manager.init_app(app)
     marshmallow.init_app(app)
+    cors.init_app(app)
 
     import app.models as MODELS
     import app.utils as UTILS
